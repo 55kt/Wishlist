6206d5cac9531b316a3e685bf39323cc78a0e231
Vlad <160039489+55kt@users.noreply.github.com>
2024-12-01 19:39:54 +0200

    First Commit

---
+++ Wishlist/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  Wishlist
+//
+//  Created by Vlad on 1/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    // MARK: - Properties
+    
+    
+    // MARK: - Body
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+// MARK: - Preview
+#Preview {
+    ContentView()
+}
---
+++ Wishlist/WishlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  WishlistApp.swift
+//  Wishlist
+//
+//  Created by Vlad on 1/12/24.
+//
+
+import SwiftUI
+
+@main
+struct WishlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
